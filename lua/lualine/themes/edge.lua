13e6d1ed474f2da7e1b783d237a6222719764e03
Sainnhe Park <sainnhe@gmail.com>
2022-04-14 09:50:44 +0800

    fix(lualine): also respect transparent background in cterm

--- lua/lualine/themes/edge.lua
+++ lua/lualine/themes/edge.lua
@@ -10,7 +10,7 @@ local configuration = vim.fn['edge#get_configuration']()
 local palette = vim.fn['edge#get_palette'](configuration.style)
 
 if configuration.transparent_background == 1 then
-  palette.bg1[1] = palette.none[1]
+  palette.bg1 = palette.none
 end
 
 return {
